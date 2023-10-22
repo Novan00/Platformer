bf63a6483bcbf72e19ed7e623cc056f50fc677d8
Novan00 <143441244+Novan00@users.noreply.github.com>
2023-10-22 17:22:10 +0300

    Update PlayerHealth.cs

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,7 +5,7 @@ public class PlayerHealth : MonoBehaviour
     private int _currentHealth = 3;
     private int _maxHealth = 3;
 
-     public int CurrentHealth => _currentHealth;
+    public int CurrentHealth => _currentHealth;
     public int MaxHealth => _maxHealth;
 
     public void TakeHeal()
