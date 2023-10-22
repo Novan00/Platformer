03873ddc8d0564841352799240cd677ae6c20504
Novan00 <143441244+Novan00@users.noreply.github.com>
2023-10-22 17:20:42 +0300

    Update PlayerHealth.cs

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int CurrentHealth => _currentHealth;
-    public int MaxHealth => _maxHealth;
-
     private int _currentHealth = 3;
     private int _maxHealth = 3;
+
+     public int CurrentHealth => _currentHealth;
+    public int MaxHealth => _maxHealth;
 
     public void TakeHeal()
     {
